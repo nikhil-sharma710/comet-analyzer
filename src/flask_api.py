cbe692b587c629bcecb0a401170fba370be9bb1a
Jason Kim <jasonthekim2002@gmail.com>
2022-05-06 17:14:46 -0500

    stuff added to flask_api

--- src/flask_api.py
+++ src/flask_api.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 
 @app.route('/read_data', methods=['POST'])
-def read_data_from_file():
+def read_data_from_file() -> str:
     """
     
     """
@@ -24,11 +24,12 @@ def read_data_from_file():
     with open('comets_data.json', 'r') as f:
         comets_data = json.load(f)
 
+
     return f'Data has been read from file\n'
 
 
 @app.route('/symbols', methods=['GET'])
-def info():
+def info() -> str:
     """
 
     """
@@ -54,8 +55,20 @@ def info():
     return describe
 
 
+@app.route('/display_data', methods=['GET'])
+def display_data() -> str:
+    """
+
+    """
+
+    logging.info('Querying route to display data')
+
+    return json.dumps(comets_data, indent=2)
+
+
+
 @app.route('/comets', methods=['GET'])
-def get_comets():
+def get_comets() -> str:
     """
 
     """
@@ -70,7 +83,7 @@ def get_comets():
 
 
 @app.route('/comets/<comet>', methods=['GET'])
-def get_comet_info(comet):
+def get_comet_info(comet) -> str:
     """
 
     """
@@ -79,16 +92,31 @@ def get_comet_info(comet):
 
     comet_dict = {}
     comet_list = []
-    comet_data = ['epoch_tdb', 'tp_tdb', 'e', 'i_deg', 'w_deg', 'node_deg', 'q_au_1', 'q_au_2', 'p_yr', 'moid_au', 'ref', 'object_name']
+    comet_data = ['object', 'epoch_tdb', 'tp_tdb', 'e', 'i_deg', 'w_deg', 'node_deg', 'q_au_1', 'q_au_2', 'p_yr', 'moid_au', 'ref', 'object_name']
 
     for item in comets_data:
-        specific_comet = item['object']
-        if comet == specific_comet:
+        if comet == item['object']:
             comet_desired = item
             for data in comet_data:
                 comet_dict[data] = comet_desired[data]
             comet_list.append(comet_dict)
     return json.dumps(comet_list, indent=2)
+
+
+
+
+
+@app.route('/test', methods=['GET'])
+def testing():
+
+    needed_data = ['object', 'epoch_tdb', 'tp_tdb', 'e', 'i_deg', 'w_deg', 'node_deg', 'q_au_1', 'q_au_2', 'p_yr', 'moid_au', 'ref', 'object_name']
+    empty_dict = {}
+    for item in comets_data:
+        if 'P/2004 R1 (McNaught)' == item['object']:    
+            for data in needed_data:
+                empty_dict[data] = item[data]
+
+    return empty_dict
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
