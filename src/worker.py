9ea953884f4136d2eb3e13a34b76e08d2b6a03cf
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2022-05-08 00:28:05 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -2,9 +2,9 @@ from jobs import q, update_job_status
 import time
 
 @q.worker
-def execute_job(jid):
-    update_job_status(jid, 'in progress')
+def execute_job(job_id):
+    update_job_status(job_id, 'in progress')
     time.sleep(15)
-    update_job_status(jid, 'complete')
+    update_job_status(job_id, 'complete')
 
 execute_job()
