2e21c525f10e94bbdfbbe43faf14a916e18b69fd
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2022-05-10 11:55:50 -0500

    Update test_flask.py

--- test/test_flask.py
+++ test/test_flask.py
@@ -8,7 +8,7 @@ import requests
 
 
 api_host = 'localhost'
-api_port = '5041'
+api_port = '5014'
 api_prefix = f'http://{api_host}:{api_port}'
 
 
@@ -21,7 +21,7 @@ def test_info():
     assert bool(re.search('Try the following routes', response.text)) == True
 
 
-def test_data_upload():
+def test_read_data_from_file():
     route = f'{api_prefix}/data'
     response = requests.post(route)
 
@@ -30,8 +30,8 @@ def test_data_upload():
     assert response.content == b'Data has been loaded to Redis from file\n'
 
 
-def test_data_read():
-    route = f'{api_prefix}/data'
+def test_get_comets():
+    route = f'{api_prefix}/read-data'
     response = requests.get(route)
 
     assert response.ok == True
@@ -39,13 +39,21 @@ def test_data_read():
 
     assert isinstance(response.json(), list) == True
     assert isinstance(response.json()[0], dict) == True
-    assert 'name' in response.json()[0].keys()
-    assert 'id' in response.json()[0].keys()
-    assert 'reclat' in response.json()[0].keys()
-    assert 'mass (g)' in response.json()[0].keys()
+    assert 'object' in response.json()[0].keys()
+    assert 'epoch_tdb' in response.json()[0].keys()
+    assert 'tp_tdb' in response.json()[0].keys()
+    assert 'e' in response.json()[0].keys()
+    assert 'i_deg' in response.json()[0].keys()
+    assert 'w_deg' in response.json()[0].keys()
+    assert 'node_deg' in response.json()[0].keys()
+    assert 'q_au_1' in response.json()[0].keys()
+    assert 'q_au_2' in response.json()[0].keys()
+    assert 'p_yr' in response.json()[0].keys()
+    assert 'moid_au' in response.json()[0].keys()
+    assert 'ref' in response.json()[0].keys()
+    assert 'object_name' in response.json()[0].keys()
 
-
-def test_jobs_info():
+def test_jobs_api():
     route = f'{api_prefix}/jobs'
     response = requests.get(route)
 
@@ -56,40 +64,39 @@ def test_jobs_info():
 
 def test_jobs_cycle():
     route = f'{api_prefix}/jobs'
-    job_data = {'start': 1, 'end': 2}
+    job_data = {'min_au': 5, 'max_au': 10, 'num_bins': 3}
     response = requests.post(route, json=job_data)
 
     assert response.ok == True
     assert response.status_code == 200
-
-    UUID = response.json()['id']
+    
+        jid = response.json()['id']
     assert isinstance(UUID, str) == True
-    assert response.json()['status'] == 'submitted'
-    assert int(response.json()['start']) == int(job_data['start'])
-    assert int(response.json()['end']) == int(job_data['end'])
-
+    assert response.json()['status'] == 'min_aued'
+    assert int(response.json()['min_au']) == int(job_data['min_au'])
+    assert int(response.json()['max_au']) == int(job_data['max_au'])
+    assert int(response.json()['num_bins']) == int(job_data['num_bins'])
 
     time.sleep(1)
-    route = f'{api_prefix}/jobs/{UUID}'
+    route = f'{api_prefix}/jobs/{jid}'
     response = requests.get(route)
 
     assert response.ok == True
     assert response.status_code == 200
 
-    assert response.json()['status'] == 'in progress'
-    assert int(response.json()['start']) == int(job_data['start'])
-    assert int(response.json()['end']) == int(job_data['end'])
-
+    assert response.json()['status'] == 'started'
+    assert int(response.json()['min_au']) == int(job_data['min_au'])
+    assert int(response.json()['max_au']) == int(job_data['max_au'])
+    assert int(response.json()['num_bins']) == int(job_data['num_bins'])
 
     time.sleep(15)
-    route = f'{api_prefix}/jobs/{UUID}'
+    route = f'{api_prefix}/jobs/{jid}'
     response = requests.get(route)
 
     assert response.ok == True
     assert response.status_code == 200
 
-    assert response.json()['status'] == 'complete'
-    assert int(response.json()['start']) == int(job_data['start'])
-    assert int(response.json()['end']) == int(job_data['end'])
-
-
+    assert response.json()['status'] == 'finished'
+    assert int(response.json()['min_au']) == int(job_data['min_au'])
+    assert int(response.json()['max_au']) == int(job_data['max_au'])
+    assert int(response.json()['num_bins']) == int(job_data['num_bins'])
